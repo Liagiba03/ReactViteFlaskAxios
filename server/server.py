50464a015ff4808d135e6dc9577cf5665649a5a9
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-11-13 23:47:49 -0600

    update server. Add cors

---
+++ server/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+# Origen de peticiones desde cualquier lugar
+cors = CORS(app, origins = '*')
+
+@app.route("/api/users", methods=["GET"])
+def users():
+    number = 54
+    return jsonify(
+        {
+            "users": [
+                "JUAN",
+                "Diana",
+                "Maria"
+            ],
+            "number": number
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
