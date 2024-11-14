1def14a2fae23e71ea208774d0d373d9999389c2
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-11-14 00:03:19 -0600

    Create endpoint->get users

--- reactFlask/src/App.jsx
+++ reactFlask/src/App.jsx
@@ -2,8 +2,20 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from "axios";
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 function App() {
-  
+  //hook users
+  const[users, setUsers] = useState([]);
+
+  //Use efffect for consult users
+  useEffect(() =>{
+    axios.get(apiUrl+"/users")
+    .then(response =>{
+      setUsers(response.data.users);
+    })
+    .catch(error => console.error("ERROR GET USERS:", error))
+  }, [])
 
   return (
     <>
