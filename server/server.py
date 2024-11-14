49104c757a8e67b0b83d364d7564338f69559160
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-11-13 23:54:46 -0600

    Add axios on react

--- server/server.py
+++ server/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 # Origen de peticiones desde cualquier lugar
+# Se sustituye * por el origen (ip)
 cors = CORS(app, origins = '*')
 
 @app.route("/api/users", methods=["GET"])
